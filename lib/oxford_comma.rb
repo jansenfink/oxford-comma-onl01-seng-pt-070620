64fa50821e4e41de027aafa9edd95fcbfed31373
Jansen Fink <jansenfink@charter.net>
2020-07-16 00:37:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
-def oxford_comma(array)
+# def oxford_comma(array)
+#   array.join(" and ")
+# end
 
+def oxford_comma(array)
+  return array.join(' and ') if array.size < 3
+  list[-1] = "and " + list[-1]
+  list.join(', ')
 end
